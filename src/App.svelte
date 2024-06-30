b92094337b4b80e004b49e48d4574fad40dbe5e3
Roger Lopez <rllopez0410@gmail.com>
2024-06-30 19:20:28 -0400

    adding new assest to test and see if the gifs work as they should and if the background fits fine

--- src/App.svelte
+++ src/App.svelte
@@ -40,7 +40,7 @@
     z-index: 1;
   }
   .profile-pic {
-    background: url(./assets/profileIcon.png);
+    background: url(./assets/profileIcon.gif);
     background-size: contain;
     height: 80px;
     width: 80px;
